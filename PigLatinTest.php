95c1aff20a5d610741a7f1d3a869a0e92e11fe34
Greg Sharpe <greg.sharpe@hospedia.co.uk>
2017-09-06 14:44:30 +0100

    Bring stand-alone tester in line with phpunit tests.

--- PigLatinTest.php
+++ PigLatinTest.php
@@ -7,26 +7,32 @@ use Gbs\Translation;
 /*
 TODO:
 
-- Add unit tests: https://phpunit.de/getting-started.html
 - Add auto load
 - Swap tabs for spaces.
 - Explain that faster more memory efficient would be to process the input serially, parsing the input string chunk-by-chunk
   and spitting out the translation.  For processing very large inputs from file this could load and process from an internal buffer
-- Error handling.
 
 */
 
-// Tests.
-$englishText1 = "This is a simple test of English to Pig Latin translation.";
-$englishText2 = " Also test that 'quote' is ok.";
+function getResultMessage($output, $expects)
+{
+	if ($output == $expects) {
+		$result = "PASS";
+	} else {
+		$result = "FAIL";
+	}
+	
+	return $result;
+}
+
 
 // Create a Pig Latin WordTranslator.
 $wordTranslator = new Translation\PLWordTranslator();
 
-// Set up and apply a PL dialect.
+// Set up and apply a custom PL dialect.
 $dialect = new Translation\Dialect();
 $dialect->separator = "";
-$dialect->vowelSuffix = "al";
+$dialect->vowelSuffix = "way";
 $dialect->consonantSuffix = "ay";
 
 $wordTranslator->setDialect($dialect);
@@ -34,18 +40,70 @@ $wordTranslator->setDialect($dialect);
 // Create a Translator using the PLWordTranslator.
 $pigLatinTranslator = new Translation\Translator($wordTranslator);
 
-// Translate some test data.
-$pigLatinText = $pigLatinTranslator->translate($englishText1);
+// Run some tests.
 
-ECHO "Test 1:\n";
-ECHO "\t$englishText1\n";
+// Test 1
+$input = "This is a simple test of English to Pig Latin translation.";
+$expects = "Isthay isway away implesay esttay ofway Englishway otay Igpay Atinlay anslationtray.";
+$output = $pigLatinTranslator->translate($input);
+$result = getResultMessage($output, $expects);
+
+ECHO "\nTest 1: $result\n";
+ECHO "\t$input\n";
 ECHO "In Pig Latin:\n";
-ECHO "\t$pigLatinText\n";
+ECHO "\t$output\n";
 
-$pigLatinText = $pigLatinTranslator->translate($englishText2);
+// Test 2
+$input = "Test that the word 'quote' is translated ok.";
+$expects = "Esttay atthay ethay ordway 'otequay' isway anslatedtray okway.";
+$output = $pigLatinTranslator->translate($input);
+$result = getResultMessage($output, $expects);
 
-ECHO "Test 2:\n";
-ECHO "\t$englishText2\n";
+ECHO "\nTest 2: $result\n";
+ECHO "\t$input\n";
 ECHO "In Pig Latin:\n";
-ECHO "\t$pigLatinText\n";
+ECHO "\t$output\n";
 
+// Test 3
+$input = "Test that the hyphenated words like 'in-laws' are translated ok.";
+$expects = "Esttay atthay ethay enatedhyphay ordsway ikelay 'inway-awslay' areway anslatedtray okway.";
+$output = $pigLatinTranslator->translate($input);
+$result = getResultMessage($output, $expects);
+
+ECHO "\nTest 3: $result\n";
+ECHO "\t$input\n";
+ECHO "In Pig Latin:\n";
+ECHO "\t$output\n";
+
+// Test 4
+$input = "";
+$expects = "";
+$output = $pigLatinTranslator->translate($input);
+$result = getResultMessage($output, $expects);
+
+ECHO "\nTest 4: $result\n";
+ECHO "\t$input\n";
+ECHO "In Pig Latin:\n";
+ECHO "\t$output\n";
+
+// Test 5
+$input = "a b c d e f g h i j k l m n o p q r s t u v w x y z";
+$expects = "away bay cay day eway fay gay hay iway jay kay lay may nay oway pay qay ray say tay uway vay way xay yay zay";
+$output = $pigLatinTranslator->translate($input);
+$result = getResultMessage($output, $expects);
+
+ECHO "\nTest 5: $result\n";
+ECHO "\t$input\n";
+ECHO "In Pig Latin:\n";
+ECHO "\t$output\n";
+
+// Test 6
+$input = "     big...  \t\t ...gaps\n       and other ws\n";
+$expects = "     igbay...  \t\t ...apsgay\n       andway otherway wsay\n";
+$output = $pigLatinTranslator->translate($input);
+$result = getResultMessage($output, $expects);
+
+ECHO "\nTest 6: $result\n";
+ECHO "\t$input\n";
+ECHO "In Pig Latin:\n";
+ECHO "\t$output\n";
